d6bf044a4f3744fe8bdaa37401a23846540bfc6c
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-05-25 10:31:01 +0200

    review of building infrastructure

--- configure.sh
+++ configure.sh
@@ -6,6 +6,7 @@ set -xe
 prefix=/usr/local
 
 ../configure \
+    --enable-maintainer-mode                    \
     --prefix="$prefix"                          \
     --config-cache                              \
     --cache-file=../config.cache                \
