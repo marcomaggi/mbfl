efbc6533413d5f57b52df24afc952b951438ad5c
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-06-05 12:45:29 +0200

    fixed makefile rule installcheck

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,11 +1,11 @@
 # setup.sh --
 
 if test -z "${mbfl_LOADED_MBFL_TEST}"
-then source "${PWD}/${MBFLTEST_LIBRARY}"
+then source "${MBFLTEST_LIBRARY:?}"
 fi
 if test -z "${mbfl_LOADED_MBFL}"
 then
-    source "${PWD}/${MBFL_LIBRARY}"
+    source "${MBFL_LIBRARY:?}"
     mbfl_main_create_exit_functions
 fi
 
