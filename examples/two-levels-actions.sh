c44714cee4e78afd19733e0f39ca4e3268211aff
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-06-05 12:03:16 +0200

    more example scripts

---
+++ examples/two-levels-actions.sh
@@ -0,0 +1,88 @@
+# two-level-actions.sh --
+
+script_PROGNAME=two-levels-actions.sh
+script_VERSION=1.0
+script_COPYRIGHT_YEARS='2013'
+script_AUTHOR='Marco Maggi'
+script_LICENSE=BSD
+script_USAGE="usage: ${script_PROGNAME} [action] [subaction] [options]"
+script_DESCRIPTION='Example script showing action arguments.'
+script_EXAMPLES=
+
+source "${MBFL_LIBRARY:=$(mbfl-config)}"
+
+mbfl_declare_action_set MAIN_ONE
+mbfl_declare_action MAIN_ONE RED  NONE red  'Do main action one red.'
+mbfl_declare_action MAIN_ONE BLUE NONE blue 'Do main action one blue.'
+
+mbfl_declare_action_set MAIN_TWO
+mbfl_declare_action MAIN_TWO GREEN NONE green 'Do main action one green.'
+mbfl_declare_action MAIN_TWO CYAN  NONE cyan  'Do main action one cyan.'
+
+mbfl_declare_action_set MAIN
+mbfl_declare_action MAIN ONE  MAIN_ONE one  'Do main action one.'
+mbfl_declare_action MAIN TWO  MAIN_TWO two  'Do main action two.'
+mbfl_declare_action MAIN HELP NONE     help 'Do main action help.'
+
+function script_before_parsing_options_RED () {
+    script_USAGE="usage: ${script_PROGNAME} red [options]"
+    script_DESCRIPTION='Example action tree: red.'
+    mbfl_declare_option ALPHA no a alpha noarg 'Enable option alpha.'
+    mbfl_declare_option BETA  '' b beta  witharg 'Set option beta.'
+}
+function script_before_parsing_options_BLUE () {
+    script_USAGE="usage: ${script_PROGNAME} blue [options]"
+    script_DESCRIPTION='Example action tree: blue.'
+    mbfl_declare_option DELTA no d delta noarg 'Enable option delta.'
+    mbfl_declare_option GAMMA '' g gamma witharg 'Set option gamma.'
+}
+function script_before_parsing_options_GREEN () {
+    script_USAGE="usage: ${script_PROGNAME} green [options]"
+    script_DESCRIPTION='Example action tree: green.'
+    mbfl_declare_option ALPHA no a alpha noarg 'Enable option alpha.'
+    mbfl_declare_option BETA  '' b beta  witharg 'Set option beta.'
+}
+function script_before_parsing_options_CYAN () {
+    script_USAGE="usage: ${script_PROGNAME} cyan [options]"
+    script_DESCRIPTION='Example action tree: cyan.'
+    mbfl_declare_option DELTA no d delta noarg 'Enable option delta.'
+    mbfl_declare_option GAMMA '' g gamma witharg 'Set option gamma.'
+}
+
+function main () {
+    mbfl_main_print_usage_screen_brief
+}
+function script_action_ONE () {
+    mbfl_main_print_usage_screen_brief
+}
+function script_action_TWO () {
+    mbfl_main_print_usage_screen_brief
+}
+function script_action_HELP () {
+    mbfl_actions_fake_action_set MAIN
+    mbfl_main_print_usage_screen_brief
+}
+
+function script_action_RED () {
+    printf 'performing action red: alpha=%s, beta=%s\n' \
+	"$script_option_ALPHA" "$script_option_beta"
+}
+function script_action_BLUE () {
+    printf 'performing action blue: delta=%s, gamma=%s\n' \
+	"$script_option_DELTA" "$script_option_GAMMA"
+}
+function script_action_GREEN () {
+    printf 'performing action green: alpha=%s, beta=%s\n' \
+	"$script_option_ALPHA" "$script_option_beta"
+}
+function script_action_CYAN () {
+    printf 'performing action cyan: delta=%s, gamma=%s\n' \
+	"$script_option_DELTA" "$script_option_GAMMA"
+}
+
+mbfl_main
+
+### end of file
+# Local Variables:
+# mode: sh
+# End:
