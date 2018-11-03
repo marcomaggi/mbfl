deaff4243e25df952ca74510793b5df8462d9fca
Marco Maggi <marco.maggi-ipsu@poste.it>
2018-11-03 07:09:54 +0100

    fixed custom configuration script

--- configure.sh
+++ configure.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 # configure.sh --
 #
-# Run this to configure.
 
 set -xe
 
 prefix=/
+if test -d /lib64
+then libdir=${prefix}/lib64
+else libdir=${prefix}/lib
+fi
 
 ../configure \
-    --enable-maintainer-mode			\
     --config-cache                              \
     --cache-file=../config.cache                \
+    --enable-maintainer-mode			\
     --prefix="${prefix}"                        \
+    --libdir="${libdir}"                        \
     "$@"
 
 ### end of file
