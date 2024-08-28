0b0b3e62a21169d9c9976a89199def15ec560a4d
Marco Maggi <mrc.mgg@gmail.com>
2024-08-28 10:26:03 +0200

    updated local configure.sh script to search the findutils in a local pathname

--- configure.sh
+++ configure.sh
@@ -4,16 +4,26 @@
 
 set -ex
 
+declare -r FINDUTILS_ROOT=/opt/findutils/4.10.0
+declare -r MBFL_PROGRAM_FIND=${FINDUTILS_ROOT}/find
+declare -r MBFL_PROGRAM_LOCATE=${FINDUTILS_ROOT}/find
+declare -r MBFL_PROGRAM_UPDATEDB=${FINDUTILS_ROOT}/updatedb
+declare -r MBFL_PROGRAM_XARGS=${FINDUTILS_ROOT}/xargs
+
 prefix=/
 
 ../configure \
-    --config-cache				\
-    --cache-file=config.cache			\
-    --enable-maintainer-mode                    \
-    --prefix="${prefix}"			\
-    --with-vc					\
-    --with-semver				\
-    --with-emacs				\
+    --config-cache						\
+    --cache-file=config.cache					\
+    --enable-maintainer-mode					\
+    --prefix="${prefix}"					\
+    --with-vc							\
+    --with-semver						\
+    --with-emacs						\
+    MBFL_PROGRAM_FIND=$MBFL_PROGRAM_FIND			\
+    MBFL_PROGRAM_LOCATE=$MBFL_PROGRAM_LOCATE			\
+    MBFL_PROGRAM_UPDATEDB=$MBFL_PROGRAM_UPDATEDB		\
+    MBFL_PROGRAM_XARGS=$MBFL_PROGRAM_XARGS			\
     "$@"
 
 ### end of file
